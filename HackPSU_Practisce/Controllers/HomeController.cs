98852d2740c95cdb45de4a8f44ac0a657c19dc0a
Ian Keefer <iantkeefer@aol.com>
2016-04-01 15:40:08 -0400

    Added quotes to about page

--- HackPSU_Practisce/Controllers/HomeController.cs
+++ HackPSU_Practisce/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HackPSU_Practisce.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,8 @@ namespace HackPSU_Practisce.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
             return View();
@@ -15,8 +18,17 @@ namespace HackPSU_Practisce.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Enter Quote";
 
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult About(string text)
+        {
+            ViewBag.Message = text;
+            db.Quotes.Add(new Quote() { Text = text });
+            db.SaveChanges();
             return View();
         }
 
